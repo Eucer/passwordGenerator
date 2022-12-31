a4892c6f15b73682c26577f0d24631858b2db5fe
Eucer <100469001+Eucer@users.noreply.github.com>
2022-12-31 19:14:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  // Definimos la longitud de la contraseña a 8 caracteres
+  const passwordLength = 23;
+
+  // Creamos una lista de caracteres que se utilizarán para generar la contraseña
+  const characterList =
+    'abc8524a334qghvbcvdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  // Generamos la contraseña de manera aleatoria
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    password += characterList[Math.floor(Math.random() * characterList.length)];
+  }
+
+  // Mostramos la contraseña generada en la pantalla
+  res.send(`La contraseña generada es: ${password}`);
+});
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {});
